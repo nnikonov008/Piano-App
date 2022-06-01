c427412224f7a02e3bd6297bce4989068dcd11a7
NNikonov <n.nikonov008@gmail.com>
2022-06-01 14:58:23 +0300

    Piano App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Piano } from './components/Piano';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Piano />
+      </header>
+    </div>
+  );
+}
+
+export default App;
